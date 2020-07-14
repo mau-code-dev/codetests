0bc04c9cbdc14078e27c662eebe683e307c2c614
mau-code-dev <52636969+mau-code-dev@users.noreply.github.com>
2020-07-13 23:00:34 -0700

    add

---
+++ js/spin.js
@@ -0,0 +1,19 @@
+<script type='text/javascript'>
+$("#mySpriteSpin").spritespin({
+  // path to the source images.
+  source: [
+  "https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png",
+  "path/to/frame_002.jpg",
+  "path/to/frame_003.jpg",
+  "path/to/frame_004.jpg",
+  "path/to/frame_005.jpg",
+  "path/to/frame_006.jpg",
+  "path/to/frame_007.jpg",
+  "path/to/frame_008.jpg",
+  "path/to/frame_009.jpg",
+  "path/to/frame_010.jpg",
+  ],
+  width   : 480,  // width in pixels of the window/frame
+  height  : 327,  // height in pixels of the window/frame
+});
+</script>
