a4480644a4e46c00a3d25c8ec64c754ee2e9fe42
mau-code-dev <52636969+mau-code-dev@users.noreply.github.com>
2020-09-17 01:28:42 -0700

    360 test

---
+++ 360-Test/data.js
@@ -0,0 +1,85 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-twintech-360-2",
+      "name": "twintech-360-2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 480,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1885753482402865,
+          "pitch": 0.05157967042181788,
+          "rotation": 0,
+          "target": "1-twintech-360-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.850345003240756,
+          "pitch": -0.0709376708292826,
+          "title": "<link rel='stylesheet' href='styles.css'><div  lass='dot' style='display: inline-block; height: 20px; width: 190px;'>AirCharge Beethoven<a href=https://www.twintechpromo.com/582-aircharge-beethoven-desktop-bluetooth-speaker-and-wireless-charging-power-station.html target='_blank'><div class='dot2' style='position: relative; float: right; order: #000 2px solid; order-radius: 25px; ackground-color: blue; eight:25px; idth: 25px;'><img rc=https://upload.wikimedia.org/wikipedia/commons/6/6d/Green_mark_dot.svg width=20></div></a></div>",
+          "text":   "<div style='text-align: center; padding: 15px 0 15px 0 ; background-color: #1E56C0; width:260px; height: 400px;'><a href=https://www.twintechpromo.com/582-aircharge-beethoven-desktop-bluetooth-speaker-and-wireless-charging-power-station.html target='_blank'>Product Page</a><div style='padding: 10px;'></div><iframe padding='100' width='260' height='215' src=https://www.youtube.com/embed/xNkEn87OaN8 frameborder='0' allow=accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture allowfullscreen></iframe><div class='dot' style='padding: 10px;'>AirCharge Beethoven</div></div>",
+        }
+      ]
+    },
+    {
+      "id": "1-twintech-360-3",
+      "name": "twintech-360-3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 480,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.569982481699947,
+          "pitch": -0.055182050463431054,
+          "rotation": 0,
+          "target": "0-twintech-360-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.8452803097131785,
+          "pitch": -0.12031398102096702,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
